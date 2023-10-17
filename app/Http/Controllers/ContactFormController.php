1b069b88716886bcb30fa8dfba1d908dc3eba14d
natsuki <su811sh1@gmail.com>
2023-10-17 09:31:25 +0900

    update: チャプター112

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -124,6 +124,9 @@ class ContactFormController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = ContactForm::find($id);
+        $contact->delete();
+
+        return to_route('contacts.index');
     }
 }
