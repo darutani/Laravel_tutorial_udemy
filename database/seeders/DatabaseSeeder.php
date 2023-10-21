b8dfc6c59b492f9cdc55225813cea0ef5b1e07a9
natsuki <su811sh1@gmail.com>
2023-10-21 23:24:34 +0900

    チャプター117

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            TestSeeder::class,
+        ]);
+
+        \App\Models\ContactForm::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
